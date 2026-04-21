e221cd2c621ae2cc8daada975de36888fb3761a0
Tim <nebukam@gmail.com>
2026-04-21 14:58:12 -0400

    Backported 5.7 updates

--- Source/PCGExElementsClusters/Public/Elements/PCGExEdgeOrder.h
+++ Source/PCGExElementsClusters/Public/Elements/PCGExEdgeOrder.h
@@ -1,4 +1,4 @@
-﻿// Copyright 2026 Timothé Lapetite and contributors
+// Copyright 2026 Timothé Lapetite and contributors
 // Released under the MIT license https://opensource.org/license/MIT/
 
 #pragma once
@@ -7,7 +7,21 @@
 
 #include "Core/PCGExClusterMT.h"
 #include "Core/PCGExClustersProcessor.h"
+#include "Sampling/PCGExSamplingCommon.h"
 #include "PCGExEdgeOrder.generated.h"
+
+namespace PCGExMT
+{
+	template<typename T>
+	class TScopedArray;
+}
+
+UENUM()
+enum class EPCGExEdgeOrderMode : uint8
+{
+	DirectionSettings = 0 UMETA(DisplayName = "Direction Settings", ToolTip="Use per-edge direction rules."),
+	DFS               = 1 UMETA(DisplayName = "DFS from Seeds", ToolTip="Orient edges by a depth-first traversal starting from external seed points. Edges are directed away from seeds (parent to child)."),
+};
 
 UCLASS(MinimalAPI, BlueprintType, ClassGroup = (Procedural), Category="PCGEx|Clusters", meta=(PCGExNodeLibraryDoc="clusters/analyze/cluster-edge-order"))
 class UPCGExEdgeOrderSettings : public UPCGExClustersProcessorSettings
@@ -21,18 +35,36 @@ public:
 	virtual FLinearColor GetNodeTitleColor() const override { return PCGEX_NODE_COLOR_NAME(ClusterGenerator); }
 #endif
 
-	virtual bool SupportsEdgeSorting() const override { return DirectionSettings.RequiresSortingRules(); }
+	virtual bool SupportsEdgeSorting() const override { return Mode == EPCGExEdgeOrderMode::DirectionSettings && DirectionSettings.RequiresSortingRules(); }
 	virtual PCGExData::EIOInit GetMainOutputInitMode() const override;
 	virtual PCGExData::EIOInit GetEdgeOutputInitMode() const override;
 
 protected:
+	virtual bool SupportsDataStealing() const override { return true; }
+	virtual TArray<FPCGPinProperties> InputPinProperties() const override;
 	virtual FPCGElementPtr CreateElement() const override;
 	//~End UPCGSettings
 
 public:
+	/** How edge endpoint order is determined. */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = Settings, meta=(PCG_NotOverridable))
+	EPCGExEdgeOrderMode Mode = EPCGExEdgeOrderMode::DirectionSettings;
+
 	/** Defines the direction in which points will be ordered to form the final paths. */
-	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = Settings, meta=(PCG_Overridable, ShowOnlyInnerProperties))
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = Settings, meta=(PCG_Overridable, EditCondition="Mode == EPCGExEdgeOrderMode::DirectionSettings", EditConditionHides))
 	FPCGExEdgeDirectionSettings DirectionSettings;
+
+	/** How seed points select their starting node in the cluster. */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = Settings, meta=(PCG_Overridable, EditCondition="Mode != EPCGExEdgeOrderMode::DirectionSettings", EditConditionHides))
+	FPCGExNodeSelectionDetails SeedPicking = FPCGExNodeSelectionDetails(200);
+
+	/** Invert the traversal direction (edges point toward seeds instead of away from them). */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = Settings, meta=(PCG_Overridable, EditCondition="Mode != EPCGExEdgeOrderMode::DirectionSettings", EditConditionHides))
+	bool bInvert = false;
+
+	/** Whether to use an octree for closest node search. Depending on your dataset, this may be faster or slower. */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = Performance, meta=(PCG_NotOverridable, AdvancedDisplay, EditCondition="Mode != EPCGExEdgeOrderMode::DirectionSettings", EditConditionHides))
+	bool bUseOctreeSearch = false;
 
 private:
 	friend class FPCGExEdgeOrderElement;
@@ -41,6 +73,8 @@ private:
 struct FPCGExEdgeOrderContext final : FPCGExClustersProcessorContext
 {
 	friend class FPCGExEdgeOrderElement;
+
+	TSharedPtr<PCGExData::FFacade> SeedsDataFacade;
 
 protected:
 	PCGEX_ELEMENT_BATCH_EDGE_DECL
@@ -57,11 +91,21 @@ protected:
 
 namespace PCGExEdgeOrder
 {
+	class FBatch;
+
 	class FProcessor final : public PCGExClusterMT::TProcessor<FPCGExEdgeOrderContext, UPCGExEdgeOrderSettings>
 	{
+		friend class FBatch;
+
 		FPCGExEdgeDirectionSettings DirectionSettings;
 		TSharedPtr<PCGExData::TBuffer<int64>> VtxEndpointBuffer;
 		TSharedPtr<PCGExData::TBuffer<int64>> EndpointsBuffer;
+
+		// DFS traversal state
+		TArray<int8> Seeded;
+		TArray<int32> Depths;
+		TSharedPtr<PCGExMT::TScopedArray<int32>> SeedNodeIndices;
+		TArray<int32> CollectedSeeds;
 
 	public:
 		FProcessor(const TSharedRef<PCGExData::FFacade>& InVtxDataFacade, const TSharedRef<PCGExData::FFacade>& InEdgeDataFacade)
@@ -75,6 +119,8 @@ namespace PCGExEdgeOrder
 		virtual bool Process(const TSharedPtr<PCGExMT::FTaskManager>& InTaskManager) override;
 		virtual void ProcessEdges(const PCGExMT::FScope& Scope) override;
 		virtual void CompleteWork() override;
+
+		void RunDFS();
 	};
 
 	class FBatch final : public PCGExClusterMT::TBatch<FProcessor>
